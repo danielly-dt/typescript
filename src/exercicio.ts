603e3172166117fc2cae699513eaa9bd509a0f15
Dani <danielly.py@gmail.com>
2026-07-14 18:00:39 -0300

    Exercício TypeScript

---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Testes
+console.log(multiplicar(5, 4)); // 20
+console.log(saudacao("Danielly")); // Olá Danielly
